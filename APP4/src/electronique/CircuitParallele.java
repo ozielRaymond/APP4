42f3e6895c15f251ee6d2384af8832e4ab8e130a
Ozi2039 <168980744+Ozi2039@users.noreply.github.com>
2026-04-02 10:05:24 -0400

    Modification de calculerResistance pour CircuitParallele

--- APP4/src/electronique/CircuitParallele.java
+++ APP4/src/electronique/CircuitParallele.java
@@ -1,16 +1,15 @@
 package electronique;
 
-import java.util.List;
-
 public class CircuitParallele extends Circuit {
 
     @Override
     public double calculerResistance() {
-        double resistance = 0;
+        double resistanceInverse = 0;
+
         for (Composant composant : composants) {
-            resistance += 1 / composant.calculerResistance();
+            resistanceInverse += 1 / composant.calculerResistance();
         }
 
-        return resistance;
+        return 1 / resistanceInverse;
     }
 }
